a1bca2135f35e4a1a9cde7ec0c2d96d656ebf6d4
SahanaJSJ <sjayaramu@eplus.com>
2019-05-10 18:18:31 +0530

    Install jdk, python pip & opencv, ngnix and unicorn libraries

--- ubuntu16.04-scripts/packer-with-shellscript/init-package.sh
+++ ubuntu16.04-scripts/packer-with-shellscript/init-package.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
 sudo apt-get install -y default-jdk
 sudo apt-get install -y python-opencv
+sudo apt install -y python-pip
 sudo apt install -y nginx
 sudo apt-get install -y unicorn
